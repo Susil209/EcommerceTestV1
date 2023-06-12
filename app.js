4bdcf9bdb8222e7ca2cee1ab06069d032600dc17
Susil <Susil.Nayak@cognizant.com>
2023-06-12 16:41:52 +0530

    vercel.json config

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 const mongoose = require("mongoose");
 
-
 main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-  console.log("Connected to DB");
+  console.log("Database connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
